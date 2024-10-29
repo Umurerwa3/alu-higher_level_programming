3a053a64c22e83810a5ff84224ef462a9eff207d
Umurerwa3 <o.umurerwa@alustudent.com>
2024-10-29 03:48:56 -0700

    indexerror

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")  # Print element without new line
-            count += 1                 # Increment count for each printed element
+            print(my_list[i], end="")
+            count +=1
         except IndexError:
-            break                      # Stop if i is out of range
-    print()  # Add a new line after printing elements
-    return count  # Return the actual number of elements printed   
+            break
+    print()
+    return count
