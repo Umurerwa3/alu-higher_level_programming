1636aa398b95032a2a1105415295e3a6cad97394
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-05 08:33:02 -0800

    Index Error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
-            count +=1
-     except:
-           pass
+            print(my_list[i], end='')
+            count += 1
+    except:
+        pass
     print()
     return count
