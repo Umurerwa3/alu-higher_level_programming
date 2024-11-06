051100df19f643bc9567c1f3e2565e005b95cea9
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-06 11:11:03 -0800

    calculate and return area using an instance method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,10 +12,9 @@ class Square:
     def __init__(self, size=0):
         """
         Initialize the square with a private instance attribute: size.
-        
+
         Args:
             size (int): The size of the square, default is 0.
-        
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
