1c34ebbd311914a26d118179f951e49b6949d74d
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-06 10:08:08 -0800

    class and raise errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,19 +2,15 @@
 """
 This module defines a Square class.
 """
-
-
 class Square:
     """
     Class that defines a square with a private instance attribute: size.
-    """
-
+    ""
     def __init__(self, size=0):
         """
         Initialize the square with a private instance attribute: size.
         Args:
             size (int): The size of the square, default is 0.
-        
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
