edec18667aa4d55eb1f6e1b1ecd0cca5b83d2122
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-09 08:34:53 -0800

    8, 9

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle based on 7-base_geometry.py.
-
-Attributes:
-    width (int): width of the rectangle.
-    height (int): height of the rectangle.
-"""
+'''Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)'''
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle"""
+    '''Rectangle class that inherits the BaseGeometry '''
 
     def __init__(self, width, height):
-        """Creates new instances of Rectangle.
-
-        Args:
-            width (int): width of rectangle.
-            height (int): height of rectangle.
-        """
+        '''A function that created a rectangle '''
+        self.integer_validator('width', width)
         self.__width = width
+        self.integer_validator('height', height)
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,42 +1,24 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle based on 8-base_geometry.py.
-
-Attributes:
-    width (int): width of the rectangle.
-    height (int): height of the rectangle.
-"""
+'''Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)'''
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle"""
+    '''Rectangle class that inherits the BaseGeometry '''
 
     def __init__(self, width, height):
-        """Creates new instances of Rectangle.
-
-        Args:
-            width (int): width of rectangle.
-            height (int): height of rectangle.
-        """
+        '''A function that created a rectangle '''
+        self.integer_validator('width', width)
         self.__width = width
+        self.integer_validator('height', height)
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
 
     def area(self):
-        """Calculates area of a rectangle.
-
-        Returns:
-            int: area.
-        """
-        return self.__width * self.__height
+        '''Function to return the area of the rectangle'''
+        return self.__height * self.__width
 
     def __str__(self):
-        """Returns string representation of the rectangle.
-
-        Returns:
-            str: string representation of rectangle.
-        """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        '''Returns [Rectangle] '''
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
