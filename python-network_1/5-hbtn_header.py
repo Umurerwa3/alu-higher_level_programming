2ba5a59810a611144e96dfbb98e596667c20023a
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-23 05:24:02 -0800

    full

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# python script that takes in a URL and an email address,
-# sends a POST request to the passed URL with the email as a parameter,
-# and finally displays the body of the response
+# python script that takes in a URL,
+# sends a request to the URL and displays the value of the,
+# variable X-Request-Id in the response header
 """
-    send a POST request to the passed URL with the email as a parameter,
-    & displaysthe body of the response
+    send a request to the URL & display value of the,
+    variable X-Request-Id in the response header
 """
 import sys
 import requests
@@ -12,7 +12,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
