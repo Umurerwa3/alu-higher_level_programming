1189f79d6d1357c88ead5bdbc26a82d087b6ef1e
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-25 05:21:32 -0800

    1,8,4

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3
-"""Defines a function add_integer(a, b=98) that adds two integers.
+"""  fetches https://alu-intranet.hbtn.io/status  """
+import urllib.request
 
-Attributes:
-    add_integer: function that adds two integers.
-"""
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = "https://alu-intranet.hbtn.io/status"
 
-
-def add_integer(a, b=98):
-    """Adds two integer and/or float values.
-
-    Args:
-        a (int): First value
-        b (int, optional): Second value. Defaults to 98.
-
-    Raises:
-        TypeError: If a and b are not integers or floats.
-
-    Returns:
-        int: Sum of a and b.
-    """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-
-    if isinstance(a, float):
-        a = int(a)
-
-    if isinstance(b, float):
-        b = int(b)
-
-    return a + b
+if __name__ == "__main__":
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
