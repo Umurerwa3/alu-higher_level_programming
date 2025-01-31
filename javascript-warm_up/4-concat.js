b68de52358d0ea5ff5324e4e5b0e84e793d8c308
Umurerwa3 <o.umurerwa@alustudent.com>
2025-01-31 04:47:50 -0800

    concatenate 2 variable with is

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-const arg1 = process.argv[2]; // First argument (if any)
-const arg2 = process.argv[3]; // Second argument (if any)
-
-if (arg1 !== undefined && arg2 !== undefined) {
-  console.log(arg1 + ' is ' + arg2);
-} else if (arg1 !== undefined) {
-  console.log(arg1);
-}
-// If no arguments, it prints nothing (which is correct).
+console.log(process.argv[2] + ' is ' + process.argv[3]);
