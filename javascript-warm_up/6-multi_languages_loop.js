7f2d7ad2a0a0b4bdf98d123e72463e7d33c802d3
Umurerwa3 <o.umurerwa@alustudent.com>
2025-01-31 05:50:27 -0800

    loop and array

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Array of strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
